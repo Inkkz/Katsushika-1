7bd16383501b1c630eee8ddd03d4d921bef7cc42
一茶 <Issamoha020@gmail.com>
2023-01-23 05:30:31 -0500

    changes

--- src/Handlers/Call.ts
+++ src/Handlers/Call.ts
@@ -6,11 +6,12 @@ export class CallHandler {
     constructor(private client: Client) {}
 
     public handleCall = async (call: WACallEvent): Promise<void> => {
+        if (call.status !== 'offer') return void null
         const caller = call.from
         const { username } = this.client.contact.getContact(caller)
         this.client.log(`${chalk.cyanBright('Call')} from ${chalk.blueBright(username)}`)
         await this.client.rejectCall(call)
-        await this.client.sendMessage(caller, { text: 'You have been banned for calling the bot fool!' })
+        await this.client.sendMessage(caller, { text: 'You are now banned' })
         await this.client.DB.banUser(caller, 'Automatic-Ban', 'DM', 'For calling the bot')
         return void (await this.client.updateBlockStatus(caller, 'block'))
     }
