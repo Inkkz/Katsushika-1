0e7d86e2165c1b63144b340d8d7d924bb65880c6
一茶 <Issamoha020@gmail.com>
2023-01-23 12:37:10 +0300

    update...

--- src/Commands/General/Info.ts
+++ src/Commands/General/Info.ts
@@ -1,28 +1,23 @@
-import { join } from 'path'
 import { BaseCommand, Command, Message } from '../../Structures'
 
 @Command('info', {
-    description: "Displays bot's info",
+    category: 'core',
+    aliases: ['alive'],
+    description: 'shows bot info',
     usage: 'info',
-    category: 'general',
-    cooldown: 10,
-    exp: 100
+    exp: 10
 })
-export default class extends BaseCommand {
-    public override execute = async ({ reply }: Message): Promise<void> => {
-        const { description, name, homepage } = require(join(__dirname, '..', '..', '..', 'package.json')) as {
-            description: string
-            homepage: string
-            name: string
-        }
-        const image = this.client.assets.get('whatsapp-bot') as Buffer
-        const uptime = this.client.utils.formatSeconds(process.uptime())
-        const text = `🌟 *WhatsApp-bot* 🌟\n\n📙 *Description: ${description}*\n\n🔗 *Commands:* ${this.handler.commands.size}\n\n🚦 *Uptime:* ${uptime}`
-        return void (await reply(image, 'image', undefined, undefined, text, undefined, {
-            title: this.client.utils.capitalize(name),
-            thumbnail: image,
-            mediaType: 1,
-            sourceUrl: homepage
-        }))
+export default class command extends BaseCommand {
+    public override execute = async (M: Message): Promise<void> => {
+    const users = await this.client.DB.user.count()
+    const uptime = this.client.utils.formatSeconds(process.uptime())
+    let getGroups = await this.client.groupFetchAllParticipating()
+    let groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
+    let res = groups.map(v=> v.id)
+    console.log(res.length)
+        
+        return void (await reply(
+            `*━━━❰ KATSUSHIKA BOT ❱━━━*\n\n🔗 *Commands:* ${this.handler.commands.size}\n\n🔮 *Groups:* ${res.length}\n\n🎐 *Users:* ${users}\n\n🚦 *Uptime:* ${uptime}`
+        ))
     }
 }
