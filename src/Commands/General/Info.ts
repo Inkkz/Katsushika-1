ffd48a1a1781111e6486fd3a6abc82fd86989f8a
一茶 <Issamoha020@gmail.com>
2023-01-24 08:58:39 +0300

    ...

--- src/Commands/General/Info.ts
+++ src/Commands/General/Info.ts
@@ -14,8 +14,7 @@ export default class command extends BaseCommand {
     let getGroups = await this.client.groupFetchAllParticipating()
     let groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
     let res = groups.map(v=> v.id)
-    console.log(res.length)
-        
+    console.log(res.length)        
         return void (await reply(
             `*━━━❰ KATSUSHIKA BOT ❱━━━*\n\n🔗 *Commands:* ${this.handler.commands.size}\n\n🔮 *Groups:* ${res.length}\n\n🎐 *Users:* ${users}\n\n🚦 *Uptime:* ${uptime}`
         ))
